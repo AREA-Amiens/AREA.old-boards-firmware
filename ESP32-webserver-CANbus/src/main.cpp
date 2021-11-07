b15a8db60b33982b766f7574b127970b6bb865b4
kingovchouffe <46623009+kingovchouffe@users.noreply.github.com>
2021-11-07 17:21:06 +0100

    Premier commit

--- ESP32-webserver-CANbus/src/main.cpp
+++ ESP32-webserver-CANbus/src/main.cpp
@@ -3,9 +3,59 @@
 #include <AsyncTCP.h>
 #include <SPIFFS.h>
 
+const char *ssid = "PA";
+const char *password = "1234";
+
+AsyncWebServer server(80);
+
 void setup() {
-  // put your setup code here, to run once:
+ Serial.begin(115200);
+ Serial.println("\n");
+
+if(!SPIFFS.begin())
+  {
+    Serial.println("Erreur SPIFFS...");
+    return;
+  }
+
+  File root = SPIFFS.open("/");
+  File file = root.openNextFile();
+
+   while(file)
+  {
+    Serial.print("File: ");
+    Serial.println(file.name());
+    file.close();
+    file = root.openNextFile();
+  }
+  WiFi.begin(ssid, password);
+	Serial.print("Tentative de connexion...");
+
+  WiFi.softAP(ssid, password);// démarrage du serveur wifi
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  server.on("/w3.css", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/w3.css", "text/css");
+  });
+
+  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/script.js", "text/javascript");
+  });
+
 }
+
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
